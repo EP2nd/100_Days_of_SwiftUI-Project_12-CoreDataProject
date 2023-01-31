fcf58f316b5b1e0a5cf959d36f91b83bcc810566
Edwin Przeźwiecki Jr <edwin.przezwiecki.jr@icloud.com>
2023-01-31 14:41:19 +0100

    Solution to the second challenge

--- CoreDataProject/FilteredList.swift
+++ CoreDataProject/FilteredList.swift
@@ -8,12 +8,17 @@
 import CoreData
 import SwiftUI
 
+/// Challenge 2:
+enum PredicateKey {
+    case equals, isMoreThan, isLessThan, includes, beginsWith
+}
+
 struct FilteredList<T: NSManagedObject, Content: View>: View {
     
     @FetchRequest var fetchRequest: FetchedResults<T>
     
     /// Challenge 1:
-    var predicate: String
+//    var predicateString: String
     
     /// This is the content closure; it'll be called once for each item in the list:
     let content: (T) -> Content
@@ -24,10 +29,26 @@ struct FilteredList<T: NSManagedObject, Content: View>: View {
         }
     }
     
-    /// Challenge 1:
-    init(predicate: String, filterKey: String, filterValue: String, @ViewBuilder content: @escaping (T) -> Content) {
-        _fetchRequest = FetchRequest<T>(sortDescriptors: [], predicate: NSPredicate(format: "%K \(predicate) %@", filterKey, filterValue))
-        self.predicate = predicate
+    /// Challenge 2:
+    static func predicateKey(_ predicate: PredicateKey) -> String {
+        switch predicate {
+        case .equals:
+            return "=="
+        case .isMoreThan:
+            return ">"
+        case .isLessThan:
+            return "<"
+        case .includes:
+            return "IN"
+        default:
+            return "BEGINSWITH"
+        }
+    }
+    
+    /// Challenge 1 and 2:
+    init(/* predicateString: String, */ filterKey: String, filterValue: String, @ViewBuilder content: @escaping (T) -> Content) {
+        _fetchRequest = FetchRequest<T>(sortDescriptors: [], predicate: NSPredicate(format: "%K \(FilteredList<T, Content>.predicateKey(.beginsWith)) %@" /* <- \(predicateString) */, filterKey, filterValue))
+//        self.predicateString = predicateString
         self.content = content
     }
 }
